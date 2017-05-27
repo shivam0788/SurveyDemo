eed768ece1c968501abab6d9edae107076b9dd7a
Deepak <deepak.saini@aardeesoft.com>
2017-05-27 15:40:10 +0530

    second commit just for test add Demo2

---
+++ app/src/main/java/store/vconnect/com/surveydemo/Demo2.java
@@ -0,0 +1,19 @@
+package store.vconnect.com.surveydemo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by user on 5/27/2017.
+ */
+
+public class Demo2 extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo);
+
+    }
+}
